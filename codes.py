0ffa4694a7da59caf69f427c62db5b5d711a61e7
Summer Wu <summer.wu82@gmail.com>
2016-03-12 05:53:32 +0100

    begin setup for aws deployment

--- codes.py
+++ codes.py
@@ -1,7 +1,23 @@
+import random
+
 # Functions that manage generation of codes
 
 # 1-time Manager Codes
 def generateManagerCode():
-	return 0
+	num = ""
+	for i in range(4):
+		num += random.choice(['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
 
-# Items
+	# To do: store final number in database
+
+	return num
+
+# Return new code if success, 0 if fail
+def checkManagerCode(manager_code):
+	# To do: check for code in database
+	if True:
+		return generateManagerCode()
+	else:
+		return 0
+
+# Items Codes
