cfa33eeae89fb6f612dae3832ceb7716ea4466a0
hpincket <harrison_pincket@brown.edu>
2016-03-12 14:37:33 +0100

    More fields

--- application.py
+++ application.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, jsonify, request
-from codes import generateCode
+from codes import generateUniqueCode
 from app import db
-from app.models import ManagerInformation, CustomerCodes
+from app.models import ManagerInformationss, ReturnedOrders
 
 app = Flask(__name__)
 
 from codes import generateManagerCode
 from sms import *
+
+BASE_URL = ("http://testhorizon.gothiagroup.com/"
+            "eCommerceServicesWebApi_ver339/api/v3/")
 
 
 @app.route('/')
@@ -22,53 +25,42 @@ def codes():
 
 @app.route('/managers/add')
 def add_manager():
+    desc = request.args.get('desc', '')
     storeName = request.args.get('storeName', '')
     storeAddress = request.args.get('storeAddress', '')
-    print(storeName)
-    print(storeAddress)
-    while True:
-        num = generateCode()
-        if ManagerInformation.query.filter_by(value=num).count() == 0:
-            db.session.add(ManagerInformation(storeName, storeAddress, num))
-            db.session.commit()
-            return num
+    num = generateUniqueCode()
+    manager = ManagerInformationss(desc, storeName, storeAddress, num)
+    print(manager)
+    db.session.add(manager)
+    db.session.commit()
+    return num
 
 
-@app.route('/customers/add')
-def add_customer():
-    while True:
-        num = generateCode()
-        if CustomerCodes.query.filter_by(value=num).count() == 0:
-            db.session.add(CustomerCodes(num))
-            db.session.commit()
-            return num
 
+@app.route('/return/<orderNumber>/<managerId>')
+def return_product(orderNumber, managerId):
+    if ReturnedOrders.query.get(orderNumber) is not None:
+        return jsonify({"Error": "Already returned this order."})
+    r = request.get(BASE_URL + "orders/"+orderNumber)
+    if r.status_code is not 200:
+        return jsonify({"Error": "Not a valid order ID"})
+    if ManagerInformationss.query.filter_by(returnCode=managerId).count() != 1:
+        return jsonify({"Error": "Manager ID not valid"})
+    # TODO: Call their API, the item has been returned
+    # Possibly via SMS.
 
-@app.route('/return/<customerId>/<managerId>')
-def return_product(customerId, managerId):
-    if CustomerCodes.query.filter_by(value=customerId).count() == 1 \
-            and ManagerInformation.query.filter_by(value=managerId).count() == 1:
-        # TODO: Call their API
-        customer = CustomerCodes.query.filter_by(value=customerId).first()
-        manager = ManagerInformation.query.filter_by(value=managerId).first()
-        db.session.delete(customer)
-        db.session.delete(manager)
-        db.session.commit()
-        res = add_manager()
-        return jsonify({"New Manager Id": res})
-    else:
-        return jsonify({"Status": False})
+    manager = ManagerInformationss.query.filter_by(returnCode=managerId).first()
+    res = generateUniqueCode()  # Not atomic at this point
+    manager.returnCode = res
 
-
-@app.route('/customers/show')
-def show_customers():
-    customers = [c.value for c in CustomerCodes.query.all()]
-    return jsonify({"customer_codes": customers})
+    db.session.add(ReturnedOrder(orderNumber))
+    db.session.commit()
+    return jsonify({"New Manager Id": res})
 
 
 @app.route('/managers/show')
 def show_managers():
-    managers = [str(m) for m in ManagerInformation.query.all()]
+    managers = [m.serialize() for m in ManagerInformationss.query.all()]
     print(managers)
     return jsonify({"manager_codes": managers})
 
@@ -80,4 +72,4 @@ def receive_sms():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', threaded=True, debug=True)
