625cee2c1868368643b15c6004bfe99853c95c64
Summer Wu <summer.wu82@gmail.com>
2016-03-12 09:17:19 +0100

    successfully connect to aws rds

--- application.py
+++ application.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, jsonify
-from app import db
-from app.models import ManagerCode
 
 app = Flask(__name__)
 
@@ -13,7 +11,7 @@ def index():
 @app.route('/codes')
 def codes():
 	manager_code = generateManagerCode()
-	return jsonify({"manager_code" : manager_code})
+	return jsonify({"manager_codes":manager_code})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- codes.py
+++ codes.py
@@ -1,4 +1,6 @@
 import random
+from app import db
+from app.models import ManagerCodes
 
 # Functions that manage generation of codes
 
@@ -9,8 +11,16 @@ def generateManagerCode():
 		num += random.choice(['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
 
 	# To do: store final number in database
+	db.session.add(ManagerCodes(num))
+	db.session.commit()
 
-	return num
+	codes = ManagerCodes.query.all()
+
+	code_list = []
+	for code in codes:
+		code_list.append(code.value)
+
+	return code_list
 
 # Return new code if success, 0 if fail
 def checkManagerCode(manager_code):
