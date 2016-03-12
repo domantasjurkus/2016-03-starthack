6741908b5d58cfdb92876d54d3b103dd60b05dfc
Summer Wu <summer.wu82@gmail.com>
2016-03-12 09:55:15 +0100

    fix code bugs

--- codes.py
+++ codes.py
@@ -13,7 +13,8 @@ def generateCode():
 
 # 1-time Manager Codes
 def generateManagerCode():
-	
+	num = generateCode()
+
 	# To do: store final number in database
 	db.session.add(ManagerCodes(num))
 	db.session.commit()
