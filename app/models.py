2bf6a6eb5902d192ab61c0dfc4bed09c3ccacbf3
Summer Wu <summer.wu82@gmail.com>
2016-03-13 08:24:53 +0100

    get web app to work

--- app/models.py
+++ app/models.py
@@ -1,16 +1,12 @@
 from app import db
 
-
 class ManagerInformationsss(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-    desc = db.Column(db.String(140))
     storeName = db.Column(db.String(70))
     storeAddress = db.Column(db.String(120))
     returnCode = db.Column(db.String(5), unique=True)
 
-    def __init__(self, desc, storeName, storeAddress, returnCode):
-        self.desc = desc
+    def __init__(self, storeName, storeAddress, returnCode):
         self.storeName = storeName
         self.storeAddress = storeAddress
         self.returnCode = returnCode
@@ -21,24 +17,24 @@ class ManagerInformationsss(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "desc": self.desc,
             "storeName": self.storeName,
             "storeAddress": self.storeAddress,
-            "returnCode": self.returnCode}
+            "returnCode": self.returnCode 
+        }
 
 
 class ReturnedOrdersss(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     orderNumber = db.Column(db.String(100))
-    lineNumber = db.Column(db.Integer())
+    itemCode = db.Column(db.String(20))
     storeAddress = db.Column(db.String(120))
     storeName = db.Column(db.String(70))
 
-    def __init__(self, orderNumber, lineNumber, storeName, storeAddress):
+    def __init__(self, orderNumber, itemCode, storeName, storeAddress):
         self.orderNumber = orderNumber
-        self.lineNumber = lineNumber
+        self.itemCode = itemCode
         self.storeName = storeName
         self.storeAddress = storeAddress
 
     def __repr__(self):
-        return '<ReturnedItems %s:%s>' % (self.orderNumber, self.lineNumber)
+        return '<ReturnedItems %s:%s>' % (self.orderNumber, self.itemCode)
